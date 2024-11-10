f46c1bd1e8b8fad56435b1259159aaa16fffb9f7
Mike Ribbons <mribbons@gmail.com>
2024-11-10 22:35:23 +1100

    add player sword

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,10 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
+var attacking = false
+var attack_delta = 0
+const ATTACK_TICKS = 0.1
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,6 +22,18 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		jump_sound.play()
 
+	if Input.is_action_just_pressed("fire"):
+		if !attacking:
+			attacking = true
+			animated_sprite.play("attack")
+	elif attacking:
+		attack_delta += delta
+		
+		if attack_delta >= ATTACK_TICKS:
+			attacking = false
+			attack_delta = 0
+	
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -27,14 +43,15 @@ func _physics_process(delta: float) -> void:
 	elif (direction < 0):
 		animated_sprite.flip_h = true
 	
-	if is_on_floor():
-		# play animations
-		if direction == 0:
-			animated_sprite.play("idle")
+	if !attacking:
+		if is_on_floor():
+			# play animations
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+			animated_sprite.play("jump")
 	
 	
 	# applies movement
